ea33b9b551a10ec32752efd86116e10fbfbc8ade
FlavioLux <42012471+FlavioLux@users.noreply.github.com>
2019-11-14 17:24:36 -0300

    Add files via upload

---
+++ Add.py
@@ -0,0 +1,13 @@
+class Add ():
+    input = 0
+    output = 0
+
+    def Add(self):
+        self.output = self.input + 4
+
+    def setAdd(self,input_final):
+        self.input = input_final
+
+    def getAdd(self):
+        return self.output
+         
