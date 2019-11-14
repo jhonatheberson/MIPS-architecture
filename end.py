beac4f0143655dbd9aeefd1ada100aaa03024681
jhonatheberson <jhonatheberson@outlook.com.br>
2019-11-14 07:29:45 -0300

    files update for class

--- end.py
+++ end.py
@@ -1,5 +1,20 @@
-def portAnd(a, b):
-    if a == b == 1:
-        return 1
-    else:
-        return 0
+class PortAnd():
+    input_1 = false
+    input_2 = false
+    output = false
+
+    def portAnd(self):
+        if self.input_1 == self.input_2 == true:
+            return true
+        else:
+            return false
+
+    def setInput_1(self, state):
+        self.input_1 = state
+
+    def setInput_2(self, state):
+        self.input_2 = state
+
+    def getAnd(self):
+        return = self.output
+
