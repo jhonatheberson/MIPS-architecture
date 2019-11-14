d878fa83a39a5830a76cb7729be51fa16b43e138
jhonatheberson <jhonatheberson@outlook.com.br>
2019-11-14 07:14:27 -0300

    files update new methodologia

--- control.py
+++ control.py
@@ -1,3 +1,64 @@
-def control(intrucion):
-    result = {"RegDst":1, "Branch":2, "MemRead":3, "MemtoReg":4, "ALUOp" = 5, "Memwrite" = 6, "ALUSrc" = 7, "RegWrite" = 8}
-    return result
+class control():
+    intrucion = []
+    RegDst = []
+    Branch = []
+    MemReadu = false
+    MemtoReg = false
+    ALUOp = false
+    Memwrite = false
+    ALUSrc = false
+    RegWrite false
+
+    def setIntrucion(self, strucion_final):
+        self.intrucion = strucion_final
+
+    def setRegDst(self, RegDst_final):
+        self.RegDst = RegDst_final
+
+    def setBranch(self, Branch_final):
+        self.Branch = Branch_final
+
+    def setMemReadu(self, state):
+        self.MemReadu = state
+
+    def setMemtoReg(self, state):
+        self.MemtoReg = state
+
+    def setALUOp(self, state):
+        self.ALUOp = state
+
+    def setMemwrite(self,state):
+        self.Memwrite = state
+
+    def setRegWrite(self, state):
+        self.RegWrite = state
+
+    def setALUSrc(self, state):
+        self.ALUSrc = state
+
+    def getIntrucion(self):
+        return = self.strucion
+
+    def getRegDst(self):
+        return = self.RegDst
+
+    def getBranch(self):
+        return = self.Branch
+
+    def getMemReadu(self):
+        return = self.MemReadu
+
+    def getMemtoReg(self):
+        return = self.MemtoReg
+
+    def getALUOp(self):
+        return = self.ALUOp
+
+    def getMemwrite(self):
+        return =  self.Memwrite
+
+    def getRegWrite(self):
+        return  = self.RegWrite
+
+    def getALUSrc(self):
+        return  =  self.ALUSrc
--- registers.py
+++ registers.py
@@ -1,2 +1,35 @@
-def refisters(RegWrite ,Read_register_1, Read_register_2, Write_register, Write_data):
-    return Read_data_1, Read_data_2
+class registers:
+    RegWrite = false
+    Read_register_1 = false
+    Read_register_2 = false
+    Write_register = false
+    Write_data = []
+    Read_data_1 = []
+    Read_data_2 = []
+
+    def setRegWrite(self, state):
+        self.RegWrite = state
+
+    def setRead_register_1(self, state):
+        self.Read_register_1 = state
+
+    def setRead_register_2(self, state):
+        self.Read_register_2 = state
+
+    def setWitre_register(self, state):
+        self.Write_register = state
+
+    def setWrite_data(self, state):
+        self.Write_data = state
+
+    def getRead_data_1(self):
+        return self.Read_data_1
+
+    def getRead_register_2(self, state):
+        return self.Read_register_2
+
+    def getWitre_register(self, state):
+        return self.Write_register
+
+    def setWrite_data(self, state):
+        return self.Write_data
